2ab5daf47727b9af933b598a0bb6cae249470d77
Brandon Silverstein <brandon@vivaimpulse.com>
2020-03-31 17:46:44 -0600

    Done with front-end for now.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,12 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require highcharts/highcharts
+//= require jquery.waypoints.min
+//= require jquery.countup.min
+
+$( document ).ready(function() {
+  $('.counter').countUp({
+    delay: 10,
+    time: 1000
+  });
+});
