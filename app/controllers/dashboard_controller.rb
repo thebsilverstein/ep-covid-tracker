c2051c86e8aa80a11224007181f6f08430b5939f
Brandon Silverstein <brandon@vivaimpulse.com>
2020-03-31 19:50:11 -0600

    Update.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -22,8 +22,8 @@ class DashboardController < ApplicationController
     end
     
     @total_cases_by_day = LazyHighCharts::HighChart.new('graph') do |f|
-      f.xAxis(title: { enabled: false }, categories: ["3/13", "3/14", "3/15", "3/16", "3/17", "3/18", "3/19", "3/20", "3/21", "3/22", "3/23", "3/24", "3/25", "3/26", "3/27", "3/28", "3/29", "3/30"])
-      f.series(name: "Total Cases", data: [1, 1, 2, 3, 3, 3, 6, 6, 6, 6, 10, 12, 21, 25, 30, 35, 40, 46])
+      f.xAxis(title: { enabled: false }, categories: ["3/13", "3/14", "3/15", "3/16", "3/17", "3/18", "3/19", "3/20", "3/21", "3/22", "3/23", "3/24", "3/25", "3/26", "3/27", "3/28", "3/29", "3/30", "3/31"])
+      f.series(name: "Total Cases", data: [1, 1, 2, 3, 3, 3, 6, 6, 6, 6, 10, 12, 21, 25, 30, 35, 40, 46, 50])
 
       f.yAxis [
         { title: { enabled: false }, allowDecimals: false },
@@ -34,8 +34,8 @@ class DashboardController < ApplicationController
     end
 
     @total_cases_by_age_range = LazyHighCharts::HighChart.new('graph') do |f|
-      f.xAxis(title: { enabled: false }, categories: ["10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89"])
-      f.series(name: "Total Cases", data: [2, 7, 10, 6, 7, 6, 1, 1])
+      f.xAxis(title: { enabled: false }, categories: ["Teens", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89"])
+      f.series(name: "Total Cases", data: [2, 9, 11, 8, 11, 6, 2, 1])
 
       f.yAxis [
         { title: { enabled: false }, allowDecimals: false },
@@ -47,7 +47,7 @@ class DashboardController < ApplicationController
 
     @total_cases_by_gender = LazyHighCharts::HighChart.new('graph') do |f|
       # f.xAxis(title: { enabled: false }, categories: ["Male", "Female"])
-      f.series(name: "Total Cases", data: [ { name: 'Male', y: 14 }, { name: 'Female', y: 26 }])
+      f.series(name: "Total Cases", data: [ { name: 'Male', y: 19 }, { name: 'Female', y: 31 }])
 
       # f.yAxis [
       #   { title: { enabled: false }, allowDecimals: false },
@@ -56,6 +56,6 @@ class DashboardController < ApplicationController
       f.chart({defaultSeriesType: "pie"})
     end
 
-    @last_updated_on = Time.zone.parse("2020-03-30 22:00:00")
+    @last_updated_on = Time.zone.parse("2020-03-31 19:48:00")
   end
 end
