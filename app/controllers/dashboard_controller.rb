44f762f83b7cf15bdab270f1723aeed19b415c76
Brandon Silverstein <brandon@vivaimpulse.com>
2020-06-15 07:48:02 -0600

    Update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -95,7 +95,7 @@ class DashboardController < ApplicationController
 
     zip_data_tick_amount = 7
 
-    @links = [ ["epstrong.org - May 21 – June 14, 2020", "http://epstrong.org/results.php"],
+    @links = [ ["epstrong.org - May 21, 2020 – #{@last_updated_on.strftime('%B %e, %Y')}", "http://epstrong.org/results.php"],
                ["City of El Paso Facebook - May 20, 2020", "https://www.facebook.com/notes/city-of-el-paso-texas-municipal-government/city-reports-two-additional-deaths/3037450162961073/"],
                ["City of El Paso Facebook - May 19, 2020", "https://www.facebook.com/notes/city-of-el-paso-texas-municipal-government/city-reports-five-additional-deaths/3034810143225075/"],
                ["City of El Paso Facebook - May 18, 2020", "https://www.facebook.com/notes/city-of-el-paso-texas-municipal-government/city-reports-additional-cases-open-texas-efforts-delayed-one-week/3032758493430240/"],
