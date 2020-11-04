97c0b0ac9f7dd2b2d0a8b654a5618442e9201f29
Brandon Silverstein <brandon@vivaimpulse.com>
2020-11-04 06:46:38 -0700

    Daily update.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,14 @@
 module DashboardHelper
+
+  def yesterday(value)
+    if value == 0
+      return "No change since yesterday"
+    end
+    str = ""
+    str += "#{number_with_delimiter(value)}"
+    str += " more than yesterday" if value > 0
+    str += " less than yesterday" if value < 0
+    str
+  end
+
 end
