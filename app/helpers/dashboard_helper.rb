555726743c5c09d084b4fa91db239a0e3618b99f
Brandon Silverstein <brandon@vivaimpulse.com>
2020-11-06 08:40:50 -0700

    Daily update.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,13 +2,13 @@ module DashboardHelper
 
   def yesterday(value, reverse = false, short = false)
     if value == 0
-      short == false ? "No change since yesterday" : 0
+      short == false ? "No change compared to yesterday" : 0
     end
     str = ""
     str += "<i class='fas fa-caret-up fa-#{reverse == false ? 'bad' : 'good'}'></i> +" if value > 0
     str += "<i class='fas fa-caret-down fa-#{reverse == false ? 'good' : 'bad'}'></i> " if value < 0
     str += "#{number_with_delimiter(value)}"
-    str += " since yesterday" if short == false
+    str += " compared to yesterday" if short == false
     str.html_safe
   end
 
